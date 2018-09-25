979fffa478e9e2d17827dcd70f6b66e772247434
TristanRoyer <Tristan.Royer@ocsbstudent.ca>
2018-09-24 23:07:03 -0400

    edited daily and weekly assignments

--- SchoolMascotsTristanR/SchoolMascotsTristanR/SchoolMascotsForm.Designer.cs
+++ SchoolMascotsTristanR/SchoolMascotsTristanR/SchoolMascotsForm.Designer.cs
@@ -28,112 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.schoolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.mniSchool1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.mniSchool2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.mniSchool3 = new System.Windows.Forms.ToolStripMenuItem();
-            this.mniSchool4 = new System.Windows.Forms.ToolStripMenuItem();
-            this.grbInformation = new System.Windows.Forms.GroupBox();
-            this.lblMascot = new System.Windows.Forms.Label();
-            this.lblSchool = new System.Windows.Forms.Label();
-            this.menuStrip1.SuspendLayout();
-            this.grbInformation.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.schoolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mniSchool1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.mniSchool2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.mniSchool3 = new System.Windows.Forms.ToolStripMenuItem();
+			this.mniSchool4 = new System.Windows.Forms.ToolStripMenuItem();
+			this.grbInformation = new System.Windows.Forms.GroupBox();
+			this.lblMascot = new System.Windows.Forms.Label();
+			this.lblSchool = new System.Windows.Forms.Label();
+			this.menuStrip1.SuspendLayout();
+			this.grbInformation.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.schoolsToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
-            this.menuStrip1.TabIndex = 0;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // schoolsToolStripMenuItem
-            // 
-            this.schoolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// schoolsToolStripMenuItem
+			// 
+			this.schoolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mniSchool1,
             this.mniSchool2,
             this.mniSchool3,
             this.mniSchool4});
-            this.schoolsToolStripMenuItem.Name = "schoolsToolStripMenuItem";
-            this.schoolsToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
-            this.schoolsToolStripMenuItem.Text = "Schools";
-            // 
-            // mniSchool1
-            // 
-            this.mniSchool1.Name = "mniSchool1";
-            this.mniSchool1.Size = new System.Drawing.Size(152, 22);
-            this.mniSchool1.Text = "Immaculata";
-            this.mniSchool1.Click += new System.EventHandler(this.mniSchool1_Click);
-            // 
-            // mniSchool2
-            // 
-            this.mniSchool2.Name = "mniSchool2";
-            this.mniSchool2.Size = new System.Drawing.Size(152, 22);
-            this.mniSchool2.Text = "MotherTeresa";
-            this.mniSchool2.Click += new System.EventHandler(this.mniSchool2_Click);
-            // 
-            // mniSchool3
-            // 
-            this.mniSchool3.Name = "mniSchool3";
-            this.mniSchool3.Size = new System.Drawing.Size(152, 22);
-            this.mniSchool3.Text = "St.Joe\'s";
-            this.mniSchool3.Click += new System.EventHandler(this.mniSchool3_Click);
-            // 
-            // mniSchool4
-            // 
-            this.mniSchool4.Name = "mniSchool4";
-            this.mniSchool4.Size = new System.Drawing.Size(152, 22);
-            this.mniSchool4.Text = "St.Mark\'s";
-            this.mniSchool4.Click += new System.EventHandler(this.mniSchool4_Click);
-            // 
-            // grbInformation
-            // 
-            this.grbInformation.Controls.Add(this.lblMascot);
-            this.grbInformation.Controls.Add(this.lblSchool);
-            this.grbInformation.Location = new System.Drawing.Point(35, 72);
-            this.grbInformation.Name = "grbInformation";
-            this.grbInformation.Size = new System.Drawing.Size(200, 100);
-            this.grbInformation.TabIndex = 1;
-            this.grbInformation.TabStop = false;
-            // 
-            // lblMascot
-            // 
-            this.lblMascot.AutoSize = true;
-            this.lblMascot.Location = new System.Drawing.Point(6, 69);
-            this.lblMascot.Name = "lblMascot";
-            this.lblMascot.Size = new System.Drawing.Size(13, 13);
-            this.lblMascot.TabIndex = 1;
-            this.lblMascot.Text = "--";
-            // 
-            // lblSchool
-            // 
-            this.lblSchool.AutoSize = true;
-            this.lblSchool.Location = new System.Drawing.Point(6, 27);
-            this.lblSchool.Name = "lblSchool";
-            this.lblSchool.Size = new System.Drawing.Size(13, 13);
-            this.lblSchool.TabIndex = 0;
-            this.lblSchool.Text = "--";
-            // 
-            // SchoolMascotsForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.grbInformation);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "SchoolMascotsForm";
-            this.Text = "School Mascots";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.grbInformation.ResumeLayout(false);
-            this.grbInformation.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.schoolsToolStripMenuItem.Name = "schoolsToolStripMenuItem";
+			this.schoolsToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+			this.schoolsToolStripMenuItem.Text = "Schools";
+			// 
+			// mniSchool1
+			// 
+			this.mniSchool1.Name = "mniSchool1";
+			this.mniSchool1.Size = new System.Drawing.Size(146, 22);
+			this.mniSchool1.Text = "Immaculata";
+			this.mniSchool1.Click += new System.EventHandler(this.mniSchool1_Click);
+			// 
+			// mniSchool2
+			// 
+			this.mniSchool2.Name = "mniSchool2";
+			this.mniSchool2.Size = new System.Drawing.Size(146, 22);
+			this.mniSchool2.Text = "MotherTeresa";
+			this.mniSchool2.Click += new System.EventHandler(this.mniSchool2_Click);
+			// 
+			// mniSchool3
+			// 
+			this.mniSchool3.Name = "mniSchool3";
+			this.mniSchool3.Size = new System.Drawing.Size(146, 22);
+			this.mniSchool3.Text = "St.Joe\'s";
+			this.mniSchool3.Click += new System.EventHandler(this.mniSchool3_Click);
+			// 
+			// mniSchool4
+			// 
+			this.mniSchool4.Name = "mniSchool4";
+			this.mniSchool4.Size = new System.Drawing.Size(146, 22);
+			this.mniSchool4.Text = "St.Mark\'s";
+			this.mniSchool4.Click += new System.EventHandler(this.mniSchool4_Click);
+			// 
+			// grbInformation
+			// 
+			this.grbInformation.Controls.Add(this.lblMascot);
+			this.grbInformation.Controls.Add(this.lblSchool);
+			this.grbInformation.Location = new System.Drawing.Point(35, 72);
+			this.grbInformation.Name = "grbInformation";
+			this.grbInformation.Size = new System.Drawing.Size(200, 100);
+			this.grbInformation.TabIndex = 1;
+			this.grbInformation.TabStop = false;
+			// 
+			// lblMascot
+			// 
+			this.lblMascot.AutoSize = true;
+			this.lblMascot.Location = new System.Drawing.Point(6, 69);
+			this.lblMascot.Name = "lblMascot";
+			this.lblMascot.Size = new System.Drawing.Size(13, 13);
+			this.lblMascot.TabIndex = 1;
+			this.lblMascot.Text = "--";
+			// 
+			// lblSchool
+			// 
+			this.lblSchool.AutoSize = true;
+			this.lblSchool.Location = new System.Drawing.Point(6, 27);
+			this.lblSchool.Name = "lblSchool";
+			this.lblSchool.Size = new System.Drawing.Size(13, 13);
+			this.lblSchool.TabIndex = 0;
+			this.lblSchool.Text = "--";
+			this.lblSchool.Click += new System.EventHandler(this.lblSchool_Click);
+			// 
+			// SchoolMascotsForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.grbInformation);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "SchoolMascotsForm";
+			this.Text = "School Mascots";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.grbInformation.ResumeLayout(false);
+			this.grbInformation.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
--- SchoolMascotsTristanR/SchoolMascotsTristanR/SchoolMascotsForm.cs
+++ SchoolMascotsTristanR/SchoolMascotsTristanR/SchoolMascotsForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Created by: Tristan Royer
+ * Created on: 23 / 09 / 18
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - SchoolMascots
+ * This program displays different schools and their mascots depending 
+ * on the choice you pick
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,27 +27,46 @@ namespace SchoolMascotsTristanR
 
         private void mniSchool1_Click(object sender, EventArgs e)
         {
-            lblSchool.Text = "Immaculata";
+			//changes the text of the school label and the mascot label
+
+			lblSchool.Text = "Immaculata";
             lblMascot.Text = "Saints";
 
         }
 
         private void mniSchool2_Click(object sender, EventArgs e)
         {
-            lblSchool.Text = "MotherTeresa"
-            lblMascot.Text = ""
+			//changes the text of the school label and the mascot label
+
+			lblSchool.Text = "MotherTeresa";
+
+			lblMascot.Text = "Spartans";
 
          
         }
 
         private void mniSchool3_Click(object sender, EventArgs e)
         {
+			//changes the text of the school label and the mascot label
+			lblSchool.Text = "St.Joe's";
 
-        }
+			lblMascot.Text = "McJagger";
+
+		}
 
         private void mniSchool4_Click(object sender, EventArgs e)
         {
+			//changes the text of the school label and the mascot label
 
-        }
-    }
+			lblSchool.Text = "St.Mark's";
+
+			lblMascot.Text = "Spartan";
+
+		}
+
+		private void lblSchool_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
 }
